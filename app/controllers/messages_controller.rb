f5de100d1d70a43cd6fee812ffe782df57f91a3f
hideto shiozawa <shiorange10592@yahoo.co.jp>
2016-07-10 14:52:47 +0000

    8

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,35 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
   def index
     @message = Message.new
     
     # Messageを全て取得する。
     @messages = Message.all  end
 
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      # メッセージが保存できなかった時
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
   end
 
   private
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end
+  
   def message_params
     params.require(:message).permit(:name, :body)
   end
