8cffafab9c2e85da7f41ee4a5f566e8bb1368aeb
hideto shiozawa <shiorange10592@yahoo.co.jp>
2016-07-12 14:30:29 +0000

    Kadai

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,6 @@ class Message < ActiveRecord::Base
     # 内容は必須入力かつ2文字以上30文字以下
     validates :body , length: { minimum:2 , maximum:30 } , presence: true
     # 内容は必須入力かつ0以上の数字
-    validates :age , numericality: 0 , presence: true
+    validates :age ,  numericality: { greater_than_or_equal_to:0, less_than_or_equal_to:100  }, presence: true
 
 end
